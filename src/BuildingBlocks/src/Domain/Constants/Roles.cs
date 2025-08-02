a0af21fe5aeae41a6351fcdf6501e0709d7ad6e4
jflaga <flaga.jeremiah@gmail.com>
2025-08-02 19:08:21 +0800

    add new roles Customer, SalesSpecialist, and SalesCoordinator

--- src/BuildingBlocks/src/Domain/Constants/Roles.cs
+++ src/BuildingBlocks/src/Domain/Constants/Roles.cs
@@ -3,4 +3,7 @@
 public abstract class Roles
 {
     public const string Administrator = nameof(Administrator);
+    public const string SalesSpecialist = nameof(SalesSpecialist);
+    public const string SalesCoordinator = nameof(SalesCoordinator);
+    public const string Customer = nameof(Customer);
 }
